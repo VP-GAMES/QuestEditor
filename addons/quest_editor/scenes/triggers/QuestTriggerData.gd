f4aec30887a82ff38858dbe8858ecf3ec3c04afa
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-04-20 13:24:00 +0200

    V 0.2.1 Add open 2D/3D scene

--- addons/quest_editor/scenes/triggers/QuestTriggerData.gd
+++ addons/quest_editor/scenes/triggers/QuestTriggerData.gd
@@ -7,6 +7,7 @@ var _data: QuestData
 onready var _type_ui = $HBoxType/Type as Label
 onready var _put_ui = $HBoxPath/Put as TextureRect
 onready var _path_ui = $HBoxPath/Path as LineEdit
+onready var _open_ui = $HBoxPath/Open as Button
 
 func set_data(data: QuestData) -> void:
 	_data = data
@@ -16,9 +17,16 @@ func set_data(data: QuestData) -> void:
 func _init_connections() -> void:
 	if not _data.is_connected("trigger_selection_changed", self, "_on_trigger_selection_changed"):
 		assert(_data.connect("trigger_selection_changed", self, "_on_trigger_selection_changed") == OK)
+	if not _open_ui.is_connected("pressed", self, "_on_open_pressed"):
+		assert(_open_ui.connect("pressed", self, "_on_open_pressed") == OK)
 
 func _on_trigger_selection_changed(trigger: QuestTrigger) -> void:
 	_selection_changed()
+
+func _on_open_pressed() -> void:
+	if _trigger and _trigger.scene:
+		_data.editor().get_editor_interface().set_main_screen_editor(_trigger.dimension)
+		_data.editor().get_editor_interface().open_scene_from_path(_trigger.scene)
 
 func _selection_changed() -> void:
 	_trigger = _data.selected_trigger()
