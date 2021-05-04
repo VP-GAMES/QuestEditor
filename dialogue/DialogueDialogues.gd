60056d74eb897f342f85649929e32f556e249253
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-04 10:14:33 +0200

    V 0.4.6 Add quest dialogue

--- dialogue/DialogueDialogues.gd
+++ dialogue/DialogueDialogues.gd
@@ -3,7 +3,8 @@
 tool
 class_name DialogueDialogues
 
+const QUEST = "f8991765-0fff-4d5c-b806-f18501d05346"
 
 const DIALOGUES = [
-
+ "Quest"
 ]
--- localization/LocalizationKeys.gd
+++ localization/LocalizationKeys.gd
@@ -5,8 +5,18 @@ class_name LocalizationKeys
 
 const BOY = "BOY"
 const GIRL = "GIRL"
+const QUEST_START_01 = "QUEST_START_01"
+const QUEST_START_02 = "QUEST_START_02"
+const QUEST_START_03 = "QUEST_START_03"
+const QUEST_START_04 = "QUEST_START_04"
+const QUEST_START_05 = "QUEST_START_05"
 
 const KEYS = [
  "BOY",
- "GIRL"
+ "GIRL",
+ "QUEST_START_01",
+ "QUEST_START_02",
+ "QUEST_START_03",
+ "QUEST_START_04",
+ "QUEST_START_05"
 ]
