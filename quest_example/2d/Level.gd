ecd9e8ea2bc291a05d1962565a57cbf8c2a6ca65
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-07 10:45:04 +0200

    V 0.5.1 Add QuestUI Button

--- quest_example/2d/Level.gd
+++ quest_example/2d/Level.gd
@@ -2,12 +2,21 @@ extends Node2D
 
 onready var _inventory_button_ui = $CanvasLayer/InventoryButton as TextureButton
 onready var _inventory_ui = $CanvasLayer/Inventory
+onready var _quest_button_ui = $CanvasLayer/QuestButton as TextureButton
+onready var _quest_ui = $CanvasLayer/Quest
 
 func _ready() -> void:
 	_inventory_button_ui.connect("pressed", self, "_on_inventory_button_pressed")
+	_quest_button_ui.connect("pressed", self, "_on_quest_button_pressed")
 
 func _on_inventory_button_pressed() -> void:
 	if _inventory_ui.visible:
 		_inventory_ui.hide()
 	else:
 		_inventory_ui.show()
+
+func _on_quest_button_pressed() -> void:
+	if _quest_ui.visible:
+		_quest_ui.hide()
+	else:
+		_quest_ui.show()
