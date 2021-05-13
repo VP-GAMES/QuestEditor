70ae24398d0527385493758a4e121663967d5fec
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-13 13:35:28 +0200

    V 0.6.0 Add dialogue for started quest

--- dialogue/DialogueDialogues.gd
+++ dialogue/DialogueDialogues.gd
@@ -5,8 +5,10 @@ class_name DialogueDialogues
 
 const QUEST = "f8991765-0fff-4d5c-b806-f18501d05346"
 const QUESTSPEAKJOHN = "2d32ccab-9668-4dd3-9e0d-560e799add0c"
+const QUESTSTARTED = "d671e9d7-09f9-413d-8515-8f1d01df85df"
 
 const DIALOGUES = [
  "Quest",
- "QuestSpeakJohn"
+ "QuestSpeakJohn",
+ "QuestStarted"
 ]
