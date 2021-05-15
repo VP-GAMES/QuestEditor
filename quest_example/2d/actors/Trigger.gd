3f256180dfe795c5b079f2c4ef9a37fc104ac022
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-16 00:03:37 +0200

    V 0.6.4 Add support for triggers in quest editor

---
+++ quest_example/2d/actors/Trigger.gd
@@ -0,0 +1,13 @@
+tool
+extends QuestTrigger2D
+
+onready var _trigger = $Trigger as Sprite
+
+func _input(event: InputEvent):
+	if inside and event.is_action_released(activate):
+		if questManager and questManager.is_quest_started():
+			var quest = questManager.started_quest()
+			var task_trigger = questManager.get_trigger_by_ui_uuid(_uuid)
+			var task = questManager.get_task_and_update_quest_state(quest, task_trigger.uuid)
+			if task.done == true:
+				_trigger.flip_h = true
