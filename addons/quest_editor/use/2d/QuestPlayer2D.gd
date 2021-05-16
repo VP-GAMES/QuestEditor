83cf62ae3afbf7086ad6400745caa1a0097988ee
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-05-16 12:52:23 +0200

    V 0.6.6 Add item to QuestEditor scene(2D)

--- addons/quest_editor/use/2d/QuestPlayer2D.gd
+++ addons/quest_editor/use/2d/QuestPlayer2D.gd
@@ -3,8 +3,13 @@ class_name QuestPlayer2D
 
 var _questManager
 const _questManagerName = "QuestManager"
+var _inventoryManager
+const _inventoryManagerName = "InventoryManager"
 
 func _ready() -> void:
 	if get_tree().get_root().has_node(_questManagerName):
 		_questManager = get_tree().get_root().get_node(_questManagerName)
 	_questManager.set_player(self)
+	if get_tree().get_root().has_node(_inventoryManagerName):
+		_inventoryManager = get_tree().get_root().get_node(_inventoryManagerName)
+	_inventoryManager.player = self
