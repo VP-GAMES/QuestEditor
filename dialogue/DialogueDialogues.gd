3ffebcf00028dc0658d9e817e1b10733b2dad7f5
Vladimir Petrenko <vladipetrenko@googlemail.com>
2021-06-01 15:12:10 +0200

    V 0.8.7 Add second quest to test

--- dialogue/DialogueDialogues.gd
+++ dialogue/DialogueDialogues.gd
@@ -7,10 +7,14 @@ const QUEST = "f8991765-0fff-4d5c-b806-f18501d05346"
 const QUESTSPEAKJOHN = "2d32ccab-9668-4dd3-9e0d-560e799add0c"
 const QUESTSTARTED = "d671e9d7-09f9-413d-8515-8f1d01df85df"
 const QUESTDELIVERY = "a696ec4d-dff6-49c3-a2c7-0f79906804e9"
+const QUESTNEXT = "652f72fc-1e98-4122-a556-71817fa9670c"
+const QUESTNEXTEND = "950769bd-2106-40d6-b14f-d70207c56318"
 
 const DIALOGUES = [
  "Quest",
  "QuestSpeakJohn",
  "QuestStarted",
- "QuestDelivery"
+ "QuestDelivery",
+ "QuestNext",
+ "QuestNextEnd"
 ]
--- quest/QuestManagerQuests.gd
+++ quest/QuestManagerQuests.gd
@@ -5,8 +5,12 @@ class_name QuestManagerQuests
 
 const QUEST_2D = "284047bd-2cb4-461f-8b85-20b9a493e584"
 const QUEST_3D = "a16c04fb-a94e-4ebc-8a62-27bd7d5a77bb"
+const QUEST_2DNEXT = "cd26efc7-d939-474d-a1ed-e29c437d990d"
+const QUEST_3DNEXT = "b28bc7b9-5ae4-470d-9991-f7fd263dec7f"
 
 const QUESTS = [
  "Quest_2D",
- "Quest_3D"
+ "Quest_3D",
+ "Quest_2DNext",
+ "Quest_3DNext"
 ]
